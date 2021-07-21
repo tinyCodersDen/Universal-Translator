4fcc6184d715b0bf262f62c9c19f7f633359dbe2
tinyCoder <65143775+tinyCodersDen@users.noreply.github.com>
2021-07-21 15:59:57 -0400

    Create Universal Translator.py

---
+++ Universal Translator.py
@@ -0,0 +1,55 @@
+'''
+Universal Speech Translator
+Author: Vihan Raval
+'''
+import speech_recognition as sr
+import pyttsx3 
+from googletrans import Translator
+# Initialize the recognizer 
+r = sr.Recognizer() 
+  
+# Function to convert text to
+# speech
+def SpeakText(command):
+      
+    # Initialize the engine
+    engine = pyttsx3.init()
+    engine.say(command) 
+    engine.runAndWait()
+      
+      
+# Loop infinitely for user to
+# speak
+  
+while(1):    
+      
+    # Exception handling to handle
+    # exceptions at the runtime
+    try:
+          
+        # use the microphone as source for input.
+        with sr.Microphone() as source2:
+              
+            # wait for a second to let the recognizer
+            # adjust the energy threshold based on
+            # the surrounding noise level 
+            r.adjust_for_ambient_noise(source2, duration=0.2)
+              
+            #listens for the user's input 
+            audio2 = r.listen(source2)
+              
+            # Using ggogle to recognize audio
+            MyText = r.recognize_google(audio2)
+            MyText = MyText.lower()
+
+            translator = Translator()
+            translation = translator.translate(MyText,dest='es')
+            print(f'{translation.origin} ({translation.src}) --> {translation.text} ({translation.dest})')
+            print("Did you say "+translation.text)
+            SpeakText(translation.text)
+              
+    except sr.RequestError as e:
+        print("Could not request results; {0}".format(e))
+          
+    except sr.UnknownValueError:
+        print("unknown error occured")
